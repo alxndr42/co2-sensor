fef1f56b671ad726ed748d6a7d30d0cd721df470
Alexander Dietrich <alexander@dietrich.cx>
2025-08-22 22:18:22 +0200

    Add temperature/humidity to hackydash

--- hackydash/script.js
+++ hackydash/script.js
@@ -1,6 +1,8 @@
 (function () {
 
-const CO2_RE = /^esphome_sensor_value\{.*id="co2".*\} (\d+)$/m;
+const CO2_RE = /^esphome_sensor_value\{.*id="co2".*unit="(.+)".*\} (\d+)$/m;
+const TEM_RE = /^esphome_sensor_value\{.*id="temperature".*unit="(.+)".*\} ([\d.]+)$/m;
+const HUM_RE = /^esphome_sensor_value\{.*id="humidity".*unit="(.+)".*\} ([\d.]+)$/m;
 
 var sensors = [];
 var limits = [];
@@ -47,34 +49,50 @@ function refreshDashboard () {
 }
 
 function updateSensor (sensor) {
-    var sensorElement = sensor.element;
-    var valueElement = sensorElement.getElementsByClassName('value')[0];
-    var xhr = new XMLHttpRequest();
+    const co2Element = sensor.element.querySelector('.co2');
+    const temElement = sensor.element.querySelector('.tem');
+    const humElement = sensor.element.querySelector('.hum');
 
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', sensor.url);
     xhr.onload = function () {
-        var match = CO2_RE.exec(xhr.responseText);
-        var level = (match === null ? -1 : Number(match[1]));
+        const text = xhr.responseText;
 
-        sensorElement.classList.remove('green', 'yellow', 'red');
+        const co2Value = updateValue(CO2_RE, text, 0, co2Element);
+        sensor.element.classList.remove('green', 'yellow', 'red');
         switch (true) {
-        case level >= limits[1]:
-            sensorElement.classList.add("red");
+        case co2Value >= limits[1]:
+            sensor.element.classList.add('red');
             break;
-        case level >= limits[0]:
-            sensorElement.classList.add("yellow");
+        case co2Value >= limits[0]:
+            sensor.element.classList.add('yellow');
             break;
-        case level >= 0:
-            sensorElement.classList.add("green");
+        case co2Value >= 0:
+            sensor.element.classList.add('green');
             break;
         }
-        valueElement.textContent = (match === null ? '---' : match[1]);
+
+        updateValue(TEM_RE, text, 1, temElement);
+        updateValue(HUM_RE, text, 1, humElement);
     };
     xhr.onerror = function () {
         sensorElement.classList.remove('green', 'yellow', 'red');
-        valueElement.textContent = '---';
+        co2Element.textContent = '---';
     };
     xhr.send();
+}
+
+function updateValue(regex, text, decimals, element) {
+    const match = regex.exec(text);
+    if (match !== null) {
+        let value = Number(match[2]).toFixed(decimals);
+        let unit = match[1];
+        element.textContent = `${value} ${unit}`;
+        return value;
+    } else {
+        element.textContent = '---';
+        return -1;
+    }
 }
 
 initialize();
